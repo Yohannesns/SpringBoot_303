45a61836e0d6fb96742d47958d9342d2e3076ede
GBTC408016UR <GBTC408016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-14 11:00:01 -0400

    first commit

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner{
+    @Autowired
+    CourseRepository repository;
+    @Override
+    public  void  run(String... strings) throws Exception{
+        Course course = new Course("Astrophysics", "Neil D Tyson",
+        "Just a course on stars", 3);
+        repository.save(course);
+
+        course  = new Course("Calculus", "Carol henley",
+            "Rate of change of the rate of change", 3);
+        repository.save(course);
+        course = new Course("FreshmanEnglish", "Geraldine pegram",
+                "Learn your language children", 3);
+        repository.save(course);
+    }
+}
